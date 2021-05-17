3e4a27a96a336cb10c1de9e2cc6ef662cbab201b
Altarriba_G <G731G@LAPTOP-5SV8D88T.mshome.net>
2021-05-17 23:21:47 +0200

    Versió 0.2 del projecte prjava01

--- src/main/java/prjava01/Main.java
+++ src/main/java/prjava01/Main.java
@@ -21,6 +21,7 @@ public class Main {
             bw.newLine();
             bw.write("  <body>");
             bw.newLine();
+            bw.write(" <h1>DAW2 m08uf4eac4</h1>");bw.newLine(); 
             bw.write("    Nova p&agrave;gina web");
             bw.newLine();
             bw.write("  </body>");
